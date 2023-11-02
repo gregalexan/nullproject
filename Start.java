ededa979546b4821b11afa55af39d364e9a4a097
Grigorios Alexandrou <t8220004@aueb.gr>
2023-11-03 00:15:38 +0200

    Package vol 2

--- Start.java
+++ Start.java
@@ -3,9 +3,17 @@ package nullproject;
 public class Start {
     public void show() {
         System.out.println("Hello");
+	Start1 s1 = new Start1();
+	s1.show1();
     }
     public static void main(String[] args) {
         Start obj = new Start();
         obj.show();
     }
 }
+class Start1 {
+	void show1() {
+		System.out.println("Hi!");
+	}
+}
+
