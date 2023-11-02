a122f349c34c17aca60ca8a1b002514856e3504a
Grigorios Alexandrou <t8220004@aueb.gr>
2023-11-02 23:23:05 +0200

    package

---
+++ Start.java
@@ -0,0 +1,11 @@
+package nullproject;
+
+public class Start {
+    public void show() {
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        Start obj = new Start();
+        obj.show();
+    }
+}
---
+++ Start2.java
@@ -0,0 +1,8 @@
+import nullproject.Start;
+
+public class Start2 {
+    public static void main(String[] args) {
+        Start s1 = new Start();
+        s1.show();
+    }
+}
